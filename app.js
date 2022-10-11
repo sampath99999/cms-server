95bae483c9e83ef5a6e17b4c3ef202b076df95e8
Sampath Bandla <sampathbandla@lewisu.edu>
2022-10-11 23:49:44 +0530

    added token verification of network

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-var bodyParser = require('body-parser')
-const cors = require('cors');
-
+var bodyParser = require("body-parser");
+const cors = require("cors");
+const networkModel = require("./models/network");
 // Router variables
 const networkRouter = require("./routers/network");
+const jwt = require("jsonwebtoken");
+const { wrongToken } = require("./includes/errors");
 
 // Initialize express
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware configuration
-dotenv.config()
-app.use(bodyParser.json())
-app.use(cors())
+dotenv.config();
+app.use(bodyParser.json());
+app.use(cors());
 
 // Connecting to DB
 dbConnect().catch((err) => {
@@ -29,6 +31,35 @@ async function dbConnect() {
 app.listen(PORT, (err) => {
     if (err) throw err;
     else console.log("Listening on port " + PORT);
+});
+
+app.use("/network", async (req, res, next) => {
+    if (req.path == "/login" || req.path == "/register") {
+    } else {
+        var token = req.body.token;
+        try {
+            // decoding token
+            var decoded = jwt.decode(token, process.env.SECRET);
+
+            // getting id from decoded
+            var network_id = decoded.id;
+
+            // checking if network exists
+            var network = await networkModel.findOne({ _id: network_id });
+            if (!network) {
+                return res.status(500).send({
+                    error: "LOGOUT",
+                    message: wrongToken,
+                });
+            }
+        } catch (err) {
+            return res.status(500).send({
+                error: "LOGOUT",
+                message: wrongToken,
+            });
+        }
+    }
+    next();
 });
 
 // Routes
