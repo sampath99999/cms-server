e69500087f7d7d4fd59acd96b358972e0bb52398
Sampath Bandla <sampathbandla@lewisu.edu>
2022-10-11 23:49:53 +0530

    added new error

--- includes/errors.js
+++ includes/errors.js
@@ -1,3 +1,4 @@
 module.exports.badCredentials = "Username or Password not found!"
 module.exports.somethingWentWrong = "Something is wrong!"
 module.exports.alreadyExists = "Username already exists!"
+module.exports.wrongToken = "Please login again!"
